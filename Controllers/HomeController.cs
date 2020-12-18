6aac62a63b8cfe3aba1ef8767a1251d3f503173b
Kirti Pawar <kirti.pawar@lntinfotech.com>
2020-12-18 11:32:24 +0530

    Azure Demo

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace WebApplication.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Kirti's description page. Updated";
 
             return View();
         }
